47b3b313824491a2078402d9afa8cc4cda50a2a5
No.1 <No.1@192.168.1.102>
2022-09-21 18:15:38 +0900

    This is commit message!

---
+++ src/kjs_project0921/DoWhileSample.java
@@ -0,0 +1,19 @@
+package kjs_project0921;
+
+public class DoWhileSample {
+
+	public static void main(String[] args) {
+		char c='a';
+		
+		do { 
+			System.out.print(c);
+			c = (char)(c+1);
+			} while(c<='z');
+		
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/kjs_project0921/Forsample.java
@@ -0,0 +1,22 @@
+package kjs_project0921;
+
+public class Forsample {
+
+	public static void main(String[] args) {
+		int sum=0;
+		for(int i=1; i<=10; i++) { 
+		sum += i;
+		System.out.print(i); 
+		if(i<=9) 
+		System.out.print("+");
+		else { 
+		System.out.print("="); 
+		System.out.print(sum); 
+		}
+		}
+
+		
+		// TODO Auto-generated method stub
+	 }
+
+}
